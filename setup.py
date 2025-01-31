5d80f5e4cb8f42887356956d0d44d0a7f8a7eaff
ankitpatil3003 <apatil@binghamton.edu>
2025-01-31 14:09:13 -0500

    requirements and installations done

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer-Project"
+AUTHOR_USER_NAME = "ankitpatil3003"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "apatil@binghamton.edu"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A simple text summarizer using BERT and Transformers.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
